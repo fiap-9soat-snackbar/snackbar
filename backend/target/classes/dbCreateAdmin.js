22a8744c12b8bed7bb94e6e1e6fecc4687f477b6
Saulo Ferreira <saulolf@gmail.com>
2024-10-15 02:20:39 -0300

    1/MongoDB uses authentication. 2/Added script to create MongoDB Admin User (dbCreateAdmin.js). 3/Application properties reconfigured and app rebuilt. 4/Healthchecks removed from DB Dockerfile and Docker Composer 5/Docker Composer adjusted for new DB authentication, container names and exposed environment variables

---
+++ backend/target/classes/dbCreateAdmin.js
@@ -0,0 +1,11 @@
+use admin
+db.createUser(
+  {
+    user: "mongodbadmin",
+    pwd: passwordPrompt(), // or cleartext password
+    roles: [
+      { role: "userAdminAnyDatabase", db: "admin" },
+      { role: "readWriteAnyDatabase", db: "admin" }
+    ]
+  }
+)
