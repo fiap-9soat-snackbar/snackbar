e665cc37b5d990d3e19c4f4272cb4c75c0ec106f
Israel Dantas <israel.dantas.ext@dasa.com.br>
2025-01-13 20:51:10 -0300

    updated checkout

--- backend/src/main/java/com/snackbar/checkout/usecase/CheckoutOrderUseCaseImpl.java
+++ backend/src/main/java/com/snackbar/checkout/usecase/CheckoutOrderUseCaseImpl.java
@@ -1,5 +1,6 @@
 package com.snackbar.checkout.usecase;
 
+import com.snackbar.basket.application.usecases.BasketUseCase;
 import com.snackbar.basket.domain.entity.Basket;
 import com.snackbar.checkout.entity.Checkout;
 import com.snackbar.checkout.gateway.CheckoutRepository;
@@ -15,38 +16,39 @@ public class CheckoutOrderUseCaseImpl implements CheckoutOrderUseCase {
 
     private final CheckoutRepository checkoutRepository;
     private final OrderService orderService;
+    private final BasketUseCase basketUseCase;
 
-
-    public CheckoutOrderUseCaseImpl(CheckoutRepository checkoutRepository, OrderService orderService) {
+    public CheckoutOrderUseCaseImpl(CheckoutRepository checkoutRepository, OrderService orderService, BasketUseCase basketUseCase) {
         this.checkoutRepository = checkoutRepository;
         this.orderService = orderService;
+        this.basketUseCase = basketUseCase;
     }
 
     @Override
     public void pay(String basketId) {
-//        Basket basket = basketOrderUseCase.findBasket(basketId);
-//
-//        Order order = new Order();
-//        order.setName(basket.name());
-//        order.setCpf(basket.cpf());
-//        order.setStatusOrder(StatusOrder.NOVO);
-//        order.setTotalPrice(basket.totalPrice());
-//
-//        order.setItems(basket.items().stream().map(item -> {
-//            com.snackbar.order.domain.model.Item orderItem = new com.snackbar.order.domain.model.Item();
-//            orderItem.setName(item.name());
-//            orderItem.setQuantity(item.quantity());
-//            return orderItem;
-//        }).collect(Collectors.toList()));
-//
-//        Order orderCreated = this.orderService.createOrder(order);
-//
-//        Checkout checkout = new Checkout();
-//        checkout.setOrderId(orderCreated.getId());
-//        checkout.setPaymentMethod("Mercado Pago");
-//        checkoutRepository.save(checkout);
+        Basket basket = basketUseCase.findBasket(basketId);
 
-        // Atualizar o status da ordem
+        Order order = new Order();
+        order.setName(basket.name());
+        order.setCpf(basket.cpf());
+        order.setStatusOrder(StatusOrder.NOVO);
+        order.setTotalPrice(basket.totalPrice());
+
+        order.setItems(basket.items().stream().map(item -> {
+            com.snackbar.order.domain.model.Item orderItem = new com.snackbar.order.domain.model.Item();
+            orderItem.setName(item.name());
+            orderItem.setQuantity(item.quantity());
+            return orderItem;
+        }).collect(Collectors.toList()));
+
+        Order orderCreated = this.orderService.createOrder(order);
+
+        Checkout checkout = new Checkout();
+        checkout.setOrderId(orderCreated.getId());
+        checkout.setPaymentMethod("Mercado Pago");
+        checkoutRepository.save(checkout);
+
+//         Atualizar o status da ordem
 //        orderService.updateStatusOrder(orderId);
     }
 }
