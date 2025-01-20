14b5fbd26e79ea40a1bc66ff474e0c2914980f51
97dantas <israeldantasleite@gmail.com>
2025-01-20 20:13:09 -0300

    adicionado payment method

--- backend/src/main/java/com/snackbar/checkout/usecase/CheckoutOrderUseCaseImpl.java
+++ backend/src/main/java/com/snackbar/checkout/usecase/CheckoutOrderUseCaseImpl.java
@@ -42,7 +42,7 @@ public class CheckoutOrderUseCaseImpl implements CheckoutOrderUseCase {
         order.setCpf(basket.cpf());
         order.setStatusOrder(StatusOrder.NOVO);
         order.setTotalPrice(basket.totalPrice());
-
+        order.setPaymentMethod("mercado_pago");
         order.setItems(basket.items().stream().map(item -> {
             OrderItem orderItem = new OrderItem();
             orderItem.setName(item.name());
